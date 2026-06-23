48436ea836fa4ff0e219f2113a7969226b272d98
poojachandana <145487073+poojachandana@users.noreply.github.com>
2026-06-24 00:59:12 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -50,9 +50,7 @@ void send_file(int client_fd, const string& path, const string& content_type) {
 
     string header =
     "HTTP/1.1 200 OK\r\n"
-    + content_type +
-    "Connection: close\r\n\r\n";
-
+    + content_type;
 write(client_fd, header.c_str(), header.length());
 
     string full_path = "public" + path;
